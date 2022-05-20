3031d84c2c25812821ac1824ec18952df554c713
auntviv <aviva.mondesir@gmail.com>
2022-05-20 10:28:18 -0500

    Auth and Nav

---
+++ src/components/nav/NavBar.js
@@ -0,0 +1,48 @@
+import React from "react";
+import { useHistory } from "react-router-dom";
+import { Link } from "react-router-dom";
+import "./NavBar.css";
+
+export const NavBar = () => {
+  const history = useHistory();
+  return (
+    <ul className="navbar">
+      <li className="navbar__item">
+        <Link className="nav-link" to="/events">
+          Events
+        </Link>
+      </li>
+      <li className="navbar__item">
+        <Link className="nav-link" to="/games">
+          Games
+        </Link>
+      </li>
+      {localStorage.getItem("lu_token") !== null ? (
+        <li className="nav-item">
+          <button
+            className="nav-link fakeLink"
+            onClick={() => {
+              localStorage.removeItem("lu_token");
+              history.push({ pathname: "/games" });
+            }}
+          >
+            Logout
+          </button>
+        </li>
+      ) : (
+        <>
+          <li className="nav-item">
+            <Link className="nav-link" to="/login">
+              Login
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/register">
+              Register
+            </Link>
+          </li>
+        </>
+      )}{" "}
+    </ul>
+  );
+};
--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import { GamerRater } from './components/GamerRater.js';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <GamerRater />
   </React.StrictMode>
 );
 
